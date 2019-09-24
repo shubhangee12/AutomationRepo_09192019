791788a3227e4556ee5696ecf7d9a3296965286a
shubhangee_m <shubhangee12@yahoo.co.in>
2019-09-24 21:50:25 +0530

    variables

---
+++ java1/src/variablesandDataTypes/firstJavaClass.java
@@ -0,0 +1,27 @@
+package variablesandDataTypes;
+
+public class firstJavaClass {
+
+	public static void main(String[] args) {
+         //widely used data types primitive and non primitive
+		int a=10;//for integer value
+		char b='A';//for single character
+		boolean c=true;//for boolen value
+		double d=6.88;//for decimal value
+		String name="ABCD";//for string  string is class
+		
+		
+		//Other data type 
+		//for numbers
+		
+		byte x=10;
+		short y=45;
+		long z=65544444;
+		
+		//for decimal values
+		float f=67.2f;
+		
+		System.out.println(a);	
+	}
+
+}
